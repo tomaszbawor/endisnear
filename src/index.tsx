f7e8776db1b78be0cf30d7921c4248d8e61d8127
Tomasz Bawor <bawortomasz@gmail.com>
2025-12-27 19:01:57 +0100

    chore: click test

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,13 @@ import "./styles/globals.css";
 import { Button } from "./components/ui/button";
 
 export const IndexPage: FC = () => {
+	const onclick = () => {
+		console.log("Test");
+	};
+
 	return (
 		<div>
-			<Button>Hello</Button>
+			<Button onClick={onclick}>Hello</Button>
 		</div>
 	);
 };
