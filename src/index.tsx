58a17f1d2eb755932cee0525458320e66a94fa13
Tomasz Bawor <bawortomasz@gmail.com>
2025-12-27 19:37:42 +0100

    chore: simple atom example

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
+import { Atom, useAtomSet, useAtomValue } from "@effect-atom/atom-react";
 import type { FC } from "react";
 
 import "./styles/globals.css";
 import { Button } from "./components/ui/button";
 
+const countValueAtom = Atom.make(0);
+
 export const IndexPage: FC = () => {
+	const countValue = useAtomValue(countValueAtom);
+	const setCountValue = useAtomSet(countValueAtom);
+
 	const onclick = () => {
-		console.log("Test");
+		setCountValue((num) => num + 1);
 	};
 
 	return (
-		<div>
-			<Button onClick={onclick}>Hello</Button>
+		<div className="container">
+			<Button onClick={onclick}>Number: {countValue}</Button>
 		</div>
 	);
 };
